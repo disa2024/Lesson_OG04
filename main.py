41d35013a6025a729b22f368e671b3a53b891d65
disa2024 <crdisa1974@gmail.com>
2024-05-27 18:53:38 +0300

    add code in main

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ list = ["камень", "ножницы", "бумага"]
 
 user_win = 0
 pc_win = 0
+
+while user_win < 3 and pc_win < 3:
+    choise_user = input("Выберите камень, ножницы или бумагу: ")
+    choise_pc = random.choice(list)
+    print(f"Вы выбрали {choise_user}, компьютер выбрал {choise_pc}")
+    if choise_user == choise_pc:
+        print("Ничья")
+    elif (choise_user == "камень" and choise_pc == "ножницы") or (choise_user == "ножницы" and choise_pc == "бумага") or (choise_user == "бумага" and choise_pc == "камень"):
+        print("Человек выиграл")
+        user_win += 1
+
